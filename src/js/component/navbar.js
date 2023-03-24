f5de3495fde606eaa17e6b04612f232675e3cffd
YanceMcFinn <fsdoubleflip@gmail.com>
2023-03-24 00:58:34 +0000

    first version

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { Context } from "../store/appContext.js"
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
+	const { store, actions } = useContext(Context)
+  return (
 		<nav className="navbar navbar-light bg-light">
-  <a className="navbar-brand" href="#">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Star_wars2.svg/1200px-Star_wars2.svg.png" width="70" alt="" />
+  <a className="navbar-brand" href="/">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Star_wars2.svg/1200px-Star_wars2.svg.png" width="100" alt="" />
   </a>
+  <button className="btn btn-primary dropdown-toggle mx-2 mb-0" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+	Favorites  </button>
+	<div className="dropdown-menu dropdown-menu-end p-2"> 
+            <ul className="px-2">
+              {store.favorites.map((favorite, index) => (
+               <li className="navbar d-flex justify-content-between dropdown-menu-item" key={index}>
+                  <div>{favorite.name}</div>
+                  <div class="deleteFav mx-2" onClick={() => actions.deleteFav(favorite.name)}>X</div>
+                </li>
+              ))}
+            </ul>
+          </div>
 </nav>
 	);
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,47 @@
-import React from "react";
-import { Card } from "/workspace/react-hello-webapp/src/js/component/card.jsx"
-import { Characters } from '/workspace/react-hello-webapp/src/js/component/characters.jsx'
+import React, { useContext } from "react";
+import { CharacterCard } from "../component/CharacterCard.jsx";
+import { PlanetCard } from "../component/PlanetCard.jsx";
+import { StarshipCard } from "../component/StarshipCard.jsx";
+import { Context } from "../store/appContext";
 import { Navbar } from '../component/navbar'
 
 import "../../styles/home.css";
+import { useParams } from "react-router";
 
-export const Home = () => (
+export const Home = () => {
+	const {store, actions} = useContext(Context)
+	const { id } = useParams()
+	const characters = store.characters
+	const planets = store.planets
+	let ships = store.ships
+	let shipImages = store.shipImages
+	let planetImages = store.planetImages
 	
+	return(
 	<div className="container-fluid">
-			<div className="row">
-				<Characters />
+			<div className="row mt-3">
+				<h2>Characters</h2>
+			</div>
+			<div className="row card-row d-flex flex-row flex-nowrap">
+				{characters?.map((character, index) => {
+					return <CharacterCard name={character.name} imageIndex ={index + 1} birth_year={character.birth_year} height={`${character.height / 100}m`} eye_color={character.eye_color} link = {"/characters/" + index} key={index} id={index} type={`characters`} />
+				})}
+			</div>
+			<div className="row mt-3">
+				<h2>Planets</h2>
+			</div>
+			<div className="row card-row d-flex flex-row flex-nowrap">
+				{planets?.map((planet, index) => {
+					return <PlanetCard name={planet.name} imageURL = {planetImages[index]} population={planet.population} climate={planet.climate} terrain={planet.terrain} link = {"/planets/" + index} key={index} />
+				})}
+			</div>
+			<div className="row mt-3">
+				<h2>Starships</h2>
+			</div>
+			<div className="row card-row d-flex flex-row flex-nowrap">
+				{ships?.map((ship, index) => {
+					return <StarshipCard imageURL = {shipImages[index]} name={ship.name} manufacturer={ship.manufacturer} cost={ship.cost_in_credits} passengers={ship.passengers} link = {"/starships/" + index} key={index} />
+				})}
 			</div>
 		</div>
-);
+)};
